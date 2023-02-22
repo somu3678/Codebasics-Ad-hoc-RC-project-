be9d15293522721b5fe4073f7cd5322b1bbeaaa9
somu3678 <103477290+somu3678@users.noreply.github.com>
2023-02-22 09:04:26 +0530

    Create Ad-Hoc Request-1.sql

---
+++ Ad-Hoc Request-1.sql
@@ -0,0 +1,9 @@
+use gdb023;
+
+###### Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region.######
+
+SELECT                
+distinct market
+FROM gdb023.dim_customer
+where customer = "Atliq Exclusive" and region = "APAC";
+
